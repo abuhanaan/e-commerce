d32fcda827e73e1916209f871eb2cf0c573de71a
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-25 23:49:29 +0100

    Getting Product Details with ActivatedRoute

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined
+  products = products
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    // getting the productId from the current route url
+    const routeParams = this.route.snapshot.paramMap
+    const productIdFromRoute = Number(routeParams.get('productId'))
+    this.product = this.products.find((product) => product.id === productIdFromRoute)
+    
+  }
 
 }
