9e3375187866f38157b7ad44f82b95447bcd41bd
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-26 10:58:39 +0100

    Displaying cart items in cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent}
+      { path: 'products/:productId', component: ProductDetailsComponent},
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -24,6 +26,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [
     AppComponent
