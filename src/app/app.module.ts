a13b5a6ac08424461d41bfa52a4e92274a065e97
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-27 15:42:36 +0100

    Leveraged materia ui & css for styling product list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
@@ -15,6 +18,8 @@ import { ShippingComponent } from './components/shipping/shipping.component';
 @NgModule({
   imports: [
     BrowserModule,
+    MatCardModule,
+    MatGridListModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
@@ -22,7 +27,8 @@ import { ShippingComponent } from './components/shipping/shipping.component';
       { path: 'products/:productId', component: ProductDetailsComponent},
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent }
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
